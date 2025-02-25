bd2d9aedddf3b6bd0ffa131b8f53892d2ed540dd
handoverhandprompting <150869385+handoverhandprompting@users.noreply.github.com>
2025-02-26 00:20:52 +0800

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -12,11 +12,11 @@ def home_content():
     Type your CHINESE homepage here
     """
     st.header('國立臺灣大學醫學院附設醫院 心臟衰竭藥事照護服務')
-    st.write('歡迎使用 一節課 互動式數位工具')
-    st.write('您知道為什麼要吃這些藥嗎')
-    st.write('您對於藥品有很多疑問嗎')
-    st.write('您想要看懂自己的抽血報告嗎')
-    st.write('您希望更瞭解自己目前心臟衰竭的情況嗎')
+    st.header('歡迎使用 一節課 ESAIC 互動式數位工具')
+    st.write('您知道為什麼要吃這些藥嗎？')
+    st.write('您對於藥品有很多疑問嗎？')
+    st.write('您想要看懂自己的抽血報告嗎？')
+    st.write('您希望更瞭解自己目前心臟衰竭的情況嗎？')
     st.write('您的需求，我們都聽到了')
 
     st.write('藥師通通都可以耐心為您解答')
